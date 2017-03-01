65c29bc6b0f7c3a1323e8c75867fb5d8d20d2706
Nirmit Patel <nap0017@auburn.edu>
2017-03-01 01:35:56 -0600

    Update UDP_Client1.java

--- UDP_Client1.java
+++ UDP_Client1.java
@@ -1,5 +1,4 @@
-package wireless;
-
+package git;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -10,6 +9,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Random;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -28,45 +28,197 @@ public class UDP_Client1 extends JFrame{
 	   public static String msg="";
 	   public static String old_msg="0";
 	   
+	   public static int ip1 = 0;
+	   public static int ip2 = 0;
+	   public static int ip3 = 0;
+	   public static int ip4 = 0;
+	   
 	 public static void main(String[] args) throws InterruptedException, IOException {
 		   UDP_Client1 client=new UDP_Client1();
 		   Scanner s = new Scanner(System.in);
+		   System.out.println("Enter IP with numbers separated by 'Enter' : ");
+		   ip1=s.nextInt();
+		   ip2=s.nextInt();
+		   ip3=s.nextInt();
+		   ip4=s.nextInt();
+		   s.nextLine();
 		   System.out.println("Enter Gremlin Function Value : ");
 		   String g=s.nextLine();
+		   //for demo handling
+		   System.out.println("Select Option to test : ");
+		   System.out.println("Option 1:All Sensors together");
+		   System.out.println("Option 2:Air Pack and Heart rate");
+		   int o = s.nextInt();
+		   //for demo handling
 		   
-		   if(c!=5){
-			 
-          	msg="0123456";
-          	int z=msg.getBytes().length;
-          	if(msg.getBytes().length!=10)
-          	{
-      		  for(int i=10;i>z;i--)
-      		  {
-      			  msg=msg+"$";
-      		  }
-      	  	 }
-          	old_msg=msg;
-          System.out.println("\nSending message packet: "+msg);
-         //for Gremlin
-		 if(!g.equals("0"))
-		 {
-	          StringBuilder sb=new StringBuilder(msg);
-	          int N = msg.length();
-	          Random r =new Random();
-	          int ig=Integer.parseInt(g);
-	          for(int i=0;i<ig;i++)
-	          {
-	        	  N = msg.length();
-	        	  sb.deleteCharAt(r.nextInt(N));
-	          }
-	          msg=sb.toString();
+		   while(c!=10){
+			
+         Random rand = new Random(); 
+         DecimalFormat two = new DecimalFormat("###.00");  
+			int heartRate = rand.nextInt(250);
+			double latitude = rand.nextDouble() * 180;
+			double longitude = rand.nextDouble() * 180;
+			int tankLevel = rand.nextInt(100);
+		
+			//for demo handling
+			Random r1 =new Random();
+	        	         
+			if(o==1)
+			{
+				String s1="HTL";
+				int N = s1.length();
+		        char c1=s1.charAt(r1.nextInt(N));
+		        if(c1=='H')
+		        {
+			        msg= "H" + heartRate ;
 
-		 }
+		        }
+		        else if(c1=='T')
+		        {
+		        	msg = "T" + tankLevel;
+		        }
+		        else
+		        {
+		        	msg = "L" + two.format(latitude )+ "N" + two.format(longitude);
+		        }
+		       // msg= "T" + tankLevel + "H" + heartRate + "L" + two.format(latitude )+ "N" + two.format(longitude);
+	            int messageLength = msg.length();
+	            String ml=Integer.toString(messageLength);
+	           messageLength +=  ml.length();
+	           ml=Integer.toString(messageLength);
+	            if(messageLength<100)
+	            {
+	            	if(messageLength<10)
+		            {
+	            		if(messageLength==9)
+	            		{
+	            			messageLength = messageLength+1;
+	            			ml="0"+(messageLength+1);
+	            		}
+	            		else
+	            		{
+	            			ml="00"+(messageLength+2);
+	            		}
+		            	
+		            }
+	            	else
+	            	{
+	            		if(messageLength==99)
+	            		{
+	            			messageLength = messageLength+1;
+	            			ml=""+messageLength;
+	            		}
+	            		else
+	            		{
+	            			ml="0"+(messageLength+1);
+	            		}
+	            	}
+	            }
+	           // messageLength +=  ml.length();
+
+	            msg = ml + msg;
+	          //	int z=msg.getBytes().length;
+			}
+			
+			else if(o==2)
+			{
+				char c1;
+				if(c==0)
+				{
+					String s1="L";
+					int N = s1.length();
+			         c1=s1.charAt(r1.nextInt(N));
+				}
+				else
+				{
+				String s1="HT";
+				int N = s1.length();
+		         c1=s1.charAt(r1.nextInt(N));
+				}
+		        if(c1=='H')
+		        {
+			        msg= "H" + heartRate ;
+
+		        }
+		        else if(c1=='T')
+		        {
+		        	msg = "T" + tankLevel;
+		        }
+		        else
+		        {
+		        	msg = "L" + two.format(latitude )+ "N" + two.format(longitude);
+		        }
+		       // msg= "T" + tankLevel + "H" + heartRate + "L" + two.format(latitude )+ "N" + two.format(longitude);
+		        int messageLength = msg.length();
+	            String ml=Integer.toString(messageLength);
+	           messageLength +=  ml.length();
+	           ml=Integer.toString(messageLength);
+	            if(messageLength<100)
+	            {
+	            	if(messageLength<10)
+		            {
+	            		if(messageLength==9)
+	            		{
+	            			messageLength = messageLength+1;
+	            			ml="0"+(messageLength+1);
+	            		}
+	            		else
+	            		{
+	            			ml="00"+(messageLength+2);
+	            		}
+		            	
+		            }
+	            	else
+	            	{
+	            		if(messageLength==99)
+	            		{
+	            			messageLength = messageLength+1;
+	            			ml=""+messageLength;
+	            		}
+	            		else
+	            		{
+	            			ml="0"+(messageLength+1);
+	            		}
+	            	}
+	            }
+	           // messageLength +=  ml.length();
+
+	            msg = ml + msg;
+			}
+			//for demo handling
+
+		/*	 
+          	msg= "T" + tankLevel + "H" + heartRate + "L" + two.format(latitude )+ "N" + two.format(longitude);
+            int messageLength = msg.length();
+            messageLength +=  Integer.toString(messageLength).length();
+            msg = messageLength + "T" + tankLevel + "H" + heartRate + "L" + two.format(latitude )+ "N" + two.format(longitude);
+          	int z=msg.getBytes().length;
+          	*/
+          	
+          	old_msg=msg;
+          	
+          System.out.println("\nSending message packet: "+msg);
+        
+          //for Gremlin
+		 //if(!g.equals("0"))
+		 //{
+	          //StringBuilder sb=new StringBuilder(msg);
+	          //int N = msg.length();
+	          //Random r =new Random();
+	          //int ig=Integer.parseInt(g);
+	          //for(int i=0;i<ig;i++)
+	          //{
+	        	  //N = msg.length();
+	        	  //sb.deleteCharAt(r.nextInt(N));
+	          //}
+	          //msg=sb.toString();
+
+		 //}
           
           client.sendPacket();
 		
 		  
-		    r=client.readyToReceivPacket();
+		  r=client.readyToReceivPacket();
 		  System.out.println("received message from server :"+r);
 		  
 		  while(r.trim().equals("0"))
@@ -74,9 +226,15 @@ public class UDP_Client1 extends JFrame{
 			  System.out.println("Sending again");
 			  byte buff[]=old_msg.getBytes();
 	            System.out.println("client side bytes :"+buff+"    "+buff.length);
+	            
+	            byte[] ipAddr = new byte[] { (byte)131, (byte)204, (byte)14, (byte)209};
+	            InetAddress address = InetAddress.getByAddress(ipAddr);
+	            System.out.println("Here " + address);
+	            
 	            DatagramPacket packetSend=
 	               new DatagramPacket(buff, buff.length,
-	               InetAddress.getLocalHost(), 12345);
+	               address, 10168);
+	               
 	            socket.send(packetSend);
 	            System.out.println("\nPacket sent");
 	            r=client.readyToReceivPacket();
@@ -100,9 +258,15 @@ public class UDP_Client1 extends JFrame{
 	        	
 	            byte buff[]=msg.getBytes();
 	            System.out.println("client side bytes :"+buff+"    "+buff.length);
+	            
+	            
+	            byte[] ipAddr = new byte[] { (byte)ip1, (byte)ip2, (byte)ip3, (byte)ip4};
+	            InetAddress addressT = InetAddress.getByAddress(ipAddr);
+	            //System.out.println("Here " + address);
+	            
 	            DatagramPacket packetSend=
 	               new DatagramPacket(buff, buff.length,
-	               InetAddress.getLocalHost(), 12345);
+	               addressT, 10168);
 	            socket.send(packetSend);
 	            System.out.println("\nPacket sent");
 	            c++;
@@ -141,7 +305,4 @@ public class UDP_Client1 extends JFrame{
   
 
 }
-
-
-
 
